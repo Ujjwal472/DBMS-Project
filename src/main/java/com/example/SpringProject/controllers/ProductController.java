a67d8226320e12d805d334268f0df118758babc9
Ujjwal S <sujjwal.cse18@itbhu.ac.in>
2020-11-14 17:34:51 +0530

    added easy update for products

--- src/main/java/com/example/SpringProject/controllers/ProductController.java
+++ src/main/java/com/example/SpringProject/controllers/ProductController.java
@@ -54,4 +54,42 @@ public class ProductController {
         return mv;
     }
 
+    @GetMapping("/decrementProductAvailable/{id}")
+    public ModelAndView decrementAv(@PathVariable(name = "id") int product_id) {
+        ModelAndView mv = new ModelAndView("redirect:/products");
+        Product product = productService.getProductById(product_id);
+        int cnt = product.getTotal_available();
+        if (cnt > 0) product.setTotal_available(cnt - 1);
+        productService.saveProduct(product);
+        return mv;
+    }
+
+    @GetMapping("/incrementProductAvailable/{id}")
+    public ModelAndView incrementAv(@PathVariable(name = "id") int product_id) {
+        ModelAndView mv = new ModelAndView("redirect:/products");
+        Product product = productService.getProductById(product_id);
+        product.setTotal_available(product.getTotal_available() + 1);
+        productService.saveProduct(product);
+        return mv;
+    }
+
+    @GetMapping("/decrementProductDefective/{id}")
+    public ModelAndView decrementDe(@PathVariable(name = "id") int product_id) {
+        ModelAndView mv = new ModelAndView("redirect:/products");
+        Product product = productService.getProductById(product_id);
+        int cnt = product.getTotal_defective();
+        if (cnt > 0) product.setTotal_defective(cnt - 1);
+        productService.saveProduct(product);
+        return mv;
+    }
+
+    @GetMapping("/incrementProductDefective/{id}")
+    public ModelAndView incrementDe(@PathVariable(name = "id") int product_id) {
+        ModelAndView mv = new ModelAndView("redirect:/products");
+        Product product = productService.getProductById(product_id);
+        product.setTotal_defective(product.getTotal_defective() + 1);
+        productService.saveProduct(product);
+        return mv;
+    }
+
 }
